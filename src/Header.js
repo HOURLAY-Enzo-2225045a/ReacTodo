9fa4c438d9fe804ed04039d398abf740c0aab3d2
Enzo HOURLAY <enzo.hourlay@etu.univ-amu.fr>
2024-03-24 16:58:20 +0100

    Add progress bar to header

--- src/Header.js
+++ src/Header.js
@@ -6,6 +6,7 @@ function Header({items}) {
         <header className="TodoApp-header">
             <h1>HEADER ICI</h1>
             <span>Il y a {items.length} Tasks ({items.filter((item) => !item.isChecked).length} en attente{(items.filter((item) => !item.isChecked).length > 1 ? "s" : "")})</span>
+            <progress value={20} max="100" />
         </header>
     );
 }
